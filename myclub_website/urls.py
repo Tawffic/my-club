914d778107d8df643518be8cc51639da619d5e19
Tawffic <96981902+Tawffic@users.noreply.github.com>
2022-11-29 22:12:11 -0800

    first commit

--- myclub_website/urls.py
+++ myclub_website/urls.py
@@ -5,4 +5,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+
 ]
+
+#configure admin
+admin.site.site_header ="My Club Administration Page"
+admin.site.site_title ="Browser Title"
+admin.site.index_title ="Welcome to the admin area"
